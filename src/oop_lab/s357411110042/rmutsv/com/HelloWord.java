3238a534504bc543fa1aad870b72a85f4dccec31
sudarat39 <beskunlasing@hotmail.com>
2017-11-19 15:19:09 +0700

    Add information

--- src/oop_lab/s357411110042/rmutsv/com/HelloWord.java
+++ src/oop_lab/s357411110042/rmutsv/com/HelloWord.java
@@ -6,6 +6,9 @@ public class HelloWord {
 public static void main(String[] args) {
 
       System.out.println("Hello World,I'am TOU");
+      System.out.println("Sudarat Kunlasing");
+      System.out.println("Information System");
+      System.out.println("MT.PMUTSV");
 
 
 
