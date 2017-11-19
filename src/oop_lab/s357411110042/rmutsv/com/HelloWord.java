e21f188d9206abdfebcc3939f86be75568d1cb31
sudarat39 <beskunlasing@hotmail.com>
2017-11-19 14:49:26 +0700

    Testing upload to githab

---
+++ src/oop_lab/s357411110042/rmutsv/com/HelloWord.java
@@ -0,0 +1,15 @@
+package oop_lab.s357411110042.rmutsv.com;
+
+public class HelloWord {
+
+//      create main_method
+public static void main(String[] args) {
+
+      System.out.println("Hello World,I'am TOU");
+
+
+
+
+}//main
+
+    }//class
