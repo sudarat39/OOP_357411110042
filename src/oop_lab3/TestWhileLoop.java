685be22f9d1da8d008ee2f7a0e9d8c102bae850f
sudarat39 <beskunlasing@hotmail.com>
2017-12-12 10:35:42 +0700

    TestSwitchCase

--- src/oop_lab3/TestWhileLoop.java
+++ src/oop_lab3/TestWhileLoop.java
@@ -1,4 +1,14 @@
 package oop_lab3;
 
 public class TestWhileLoop {
+    public static void main (String[] args) {
+        //while
+        int i= 1;
+        while (i<=10){
+            System.out.print (i+" ");
+            i++;
+
+        }//while
+
+    }
 }
