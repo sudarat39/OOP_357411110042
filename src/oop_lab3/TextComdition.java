7edf3af67f38384ca174b487ac2e98c0fbfbc68d
sudarat39 <beskunlasing@hotmail.com>
2017-12-12 10:27:22 +0700

    TestCalGrade

---
+++ src/oop_lab3/TextComdition.java
@@ -0,0 +1,30 @@
+package oop_lab3;
+
+public class TextComdition {
+    public static void main(String[] args) {
+        //IF
+        int x = 10, y = 20;
+        if (x > y || x + 15 > y) ;
+        System.out.println("x more then y");
+        System.out.println("OOP");
+        //  IF-else
+        if (x > y) {
+            if (x > y) {
+                System.out.println("x mare then y");
+            } else {
+                System.out.println("x less then y");
+
+            }
+            // IF-else-if
+            if (x < 10) {
+                System.out.println("X less than 10");
+            } else if (x > 10) {
+                System.out.println("x more then 10");
+            } else {
+                System.out.println("x is 10");
+
+            }//main
+        }
+    }
+}
+
