ccf37f47e9d8a795b95dab2057a7dbde27ed8ea8
beskunlasing@hotmail.com <sudarat39>
2018-01-16 10:37:43 +0700

    midterm

---
+++ src/oop_lab6/midterm.java
@@ -0,0 +1,17 @@
+package oop_lab6;
+import java.util.Scanner;
+
+public class midterm{
+
+    private static final int x=2561;
+    public static void main(String[] args) {
+        int i = 1;
+        for (; i <= 3; ){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("กรุณาใส่ปี พ.ศ. เกิดของคุณ: ");
+            int y = scanner.nextInt();
+            System.out.println("อายุของคุณคือ: "+(x-y));
+            i++;
+        }
+    }
+}
