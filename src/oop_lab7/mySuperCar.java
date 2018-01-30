c469ec1bb6d5cacb0cdff32b4071438eebc8f12f
sudarat39 <beskunlasing@hotmail.com>
2018-01-30 11:20:50 +0700

    mySuperCar

---
+++ src/oop_lab7/mySuperCar.java
@@ -0,0 +1,30 @@
+package oop_lab7;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class mySuperCar {
+    public static void main(String[] args) {
+        ArrayList<SuperCar> myCarList = new ArrayList<SuperCar>();
+        myCarList = inputData(myCarList);
+        System.out.println("== Show Super Car Info ==");
+        for (int i=0;i<myCarList.size();i++){
+            System.out.println(myCarList.get(i).getSuperCarInfo());
+        }
+    }
+    private static ArrayList inputData(ArrayList myCarList) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert super car info: ");
+        for (int i =0;i<2;i++){
+            System.out.println("Super car: "+(i+1));
+            System.out.print("Brand: ");
+            String b = scanner.nextLine();
+            System.out.print("Color: ");
+            String c = scanner.nextLine();
+            System.out.print("Engine Size: ");
+            String e = scanner.nextLine();
+            SuperCar car  = new SuperCar(b,c,e);
+            myCarList.add(car);
+        }
+        return myCarList;
+    }
+
+}
