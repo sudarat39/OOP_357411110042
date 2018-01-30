453db625fc9800dfbc7604cfef4d337f9db13a9d
sudarat39 <beskunlasing@hotmail.com>
2018-01-30 09:46:06 +0700

    TestMethods

--- src/oop_lab8/PersonInfo.java
+++ src/oop_lab8/PersonInfo.java
@@ -1,4 +1,11 @@
 package oop_lab8;
 
 public class PersonInfo {
+public static void main (String [] args){
+String student = new Student(
+        personID:"1111111111111",
+        name: "Boy Saiyai",
+
+
+)}
 }
