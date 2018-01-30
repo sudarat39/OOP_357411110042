3c05451d5249efcdc836fcd96a52c4b27e135f6c
sudarat39 <beskunlasing@hotmail.com>
2018-01-30 09:15:47 +0700

    TestInput

---
+++ src/oop_lab8/Person.java
@@ -0,0 +1,20 @@
+package oop_lab8;
+
+public class Person {
+    private String personID;
+    private String name;
+    private String gender;
+    private int age;
+
+    public Person(String personID, String name, String gender, int age) {
+        this.personID = personID;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+    //constructor
+
+
+
+    }
+} //class
