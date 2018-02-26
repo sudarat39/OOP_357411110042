e1816ab21e830c3529b5c39675bbbf9d975d8a94
sudarat39 <beskunlasing@hotmail.com>
2018-02-26 13:36:38 +0700

    myArrayList

---
+++ src/oop_lab5/myArrayList.java
@@ -0,0 +1,25 @@
+package oop_lab5;
+
+import java.util.ArrayList;
+
+public class myArrayList {
+    public static void main(String[] args) {
+
+        //ArrayList
+        ArrayList list = new ArrayList();
+        list.add("TOU");
+        list.add("NAT");
+        list.add("Girl");
+        System.out.println(list);
+        list.add(2, 2017);
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+        System.out.println(list.size());
+        list.remove("Girl");
+        System.out.println(list);
+        System.out.println(list.get(1));
+        System.out.println(list.indexOf(2017));
+
+    }//main
+}//class
